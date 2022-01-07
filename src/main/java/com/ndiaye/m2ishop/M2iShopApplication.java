f5e65f7197858a765e50e7769663c099ffd69e52
Ndiaye <diabir_n@etna-alternance.net>
2022-01-07 14:48:10 +0100

    Initial sample project with dependencies

---
+++ src/main/java/com/ndiaye/m2ishop/M2iShopApplication.java
@@ -0,0 +1,13 @@
+package com.ndiaye.m2ishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2iShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2iShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndiaye/m2ishop/M2iShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndiaye.m2ishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2iShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
